b4c10df1d7e1f895f2d98f6a6cadb1f5fe01b6f3
Yulya Ruban <yuvshevchuk@gmail.com>
2023-06-19 12:03:51 +0300

    change sql

--- Script-1.sql
+++ Script-1.sql
@@ -1,6 +1,6 @@
 create table if not exists Genre (
 	genre_id SERIAL primary key,
-	genre_name varchar(60) not null,
+	genre_name varchar(60) unique,
 	description text)
 		
 
@@ -12,7 +12,7 @@ create table if not exists Artist (
 create table if not exists Album (
 	album_id serial primary key,
 	album_name varchar(60) not null,
-	album_year_of_issue year not null)
+	album_year_of_issue integer(4) )
 
 create table if not exists GenreArtist (
 	genre_id integer references Genre(genre_id),
@@ -27,14 +27,16 @@ create table if not exists AlbumArtist (
 create table if not exists Track (
 	track_id serial primary key,
 	track_name varchar(60) not null,
-	track_duration time not null)
+	track_duration integer not null,
+	album_id integer references Album(album_id))
 
 create table if not exists Collection_of_songs(
 	coll_of_songs_id serial primary key,
 	collection_name varchar(100) not null,
-	collection_year integer not null )
+	collection_year integer(4) )
 	
 create table if not exists TrackCollection (
 	coll_of_songs_id integer references Collection_of_songs(coll_of_songs_id),
 	track_id integer references Track(track_id),
-	constraint pk primary key (coll_of_songs_id, track_id))
+	constraint pk primary key (coll_of_songs, track_id)
+	)
